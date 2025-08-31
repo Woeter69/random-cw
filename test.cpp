2353914997c261e48f0a85f08cb5f61fc0d63e20
Woeter <skippedlead@gmail.com>
2025-08-31 13:56:44 +0530

    Was trying something

--- test.cpp
+++ test.cpp
@@ -1,41 +1,41 @@
-// //#include<iostream>
-// //using namespace std;
-// //
-// //int main() {
-// //    int time;
-// //    cout << "Enter the time in 24 hour format: " << endl;
-// //    cin >> time;
-// //    
-// //    if (time < 12 && time >= 0) {
-// //        cout << "Morning";
-// //    }
-// //    else if (time >= 12 && time < 18) {
-// //        cout << "Afternoon";
-// //    }
-// //    else {
-// //        cout << "Night";
-// //    }
-// //    
-// //    return 0;
-// //}
+/* #include<iostream> */
+/* using namespace std; */
 
-// #include<iostream>
-// using namespace std;
+/* int main() { */
+/*     int time; */
+/*     cout << "Enter the time in 24 hour format: " << endl; */
+/*     cin >> time; */
+    
+/*     if (time < 12 && time >= 0) { */
+/*         cout << "Morning"; */
+/*     } */
+/*     else if (time >= 12 && time < 18) { */
+/*         cout << "Afternoon"; */
+/*     } */
+/*     else { */
+/*         cout << "Night"; */
+/*     } */
+    
+/*     return 0; */
+/* } */
 
-// int main() {
-//     int a,b;
-//     cout << "Enter the first number: " << endl;
-//     cin >> a;
-//     cout << "Enter the second number: " << endl;
-//     cin >> b;
-//     if (b==0) {
-//         cout << "Error: Division by zero" << endl;
-//     }
-//     else {
-//         cout << "The division of the two numbers is: " << a/b << endl;
-//     }
-//     return 0;
-// }
+/* #include<iostream> */
+/* using namespace std; */
+
+/* int main() { */
+/*     int a,b; */
+/*     cout << "Enter the first number: " << endl; */
+/*     cin >> a; */
+/*     cout << "Enter the second number: " << endl; */
+/*     cin >> b; */
+/*     if (b==0) { */
+/*         cout << "Error: Division by zero" << endl; */
+/*     } */
+/*     else { */
+/*         cout << "The division of the two numbers is: " << a/b << endl; */
+/*     } */
+/*     return 0; */
+/* } */
 #include<iostream>
 using namespace std;
 int n = 5;
