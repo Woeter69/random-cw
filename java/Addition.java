1e6243f7af35497ae7602cdbc804054dd6b84e3c
Woeter <skippedlead@gmail.com>
2026-01-20 11:15:24 +0530

    :D

---
+++ java/Addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Addition {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    int a = scanner.nextInt();
+    System.out.print("Enter second number: ");
+    int b = scanner.nextInt();
+
+    System.out.println("Addition = " + (a+b) );
+  }
+}
