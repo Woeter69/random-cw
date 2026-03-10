b2e348d12665311da085e6258b84d31d7db905c2
Woeter <skippedlead@gmail.com>
2026-03-10 07:53:32 +0530

    Fixed it

--- java/Assignment_2/Assignment_2_162502.java
+++ java/Assignment_2/Assignment_2_162502.java
@@ -45,17 +45,15 @@ class thisKeyword {
     }
 }
 
-class objParameter {
-    public static class Greeting {
-        String name;
+class Greeting {
+    String name;
 
-        Greeting(String name) {
-            this.name = name;
-        }
+    Greeting(String name) {
+        this.name = name;
+    }
 
-        public void Greet(Greeting person) {
-            System.out.println("Hello, " + person.name);
-        }
+    public void Greet(Greeting person) {
+        System.out.println("Hello, " + person.name);
     }
 }
 
@@ -86,68 +84,82 @@ class Fibonacci {
     }
 }
 
+class Outer {
+    String name;
+    class Inner {
+        void Shout(String name) {
+            Outer.this.name = name;
+            System.out.println(name.toUpperCase() + "!!!");
+        }
+    }
+}
+
 public class Assignment_2_162502 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
+      Scanner scanner = new Scanner(System.in);
+      int choice = -1;
+
+      if (args.length > 0) {
+        choice = Integer.parseInt(args[0]);
+        
+        if (choice < 0 || choice > 9) {
+            System.out.println("Argument out of range (0-9). Loading menu...");
+            choice = -1;
+        }
+      }
 
         do {
-            System.out.println("\n===== MENU =====");
-            System.out.println("1. Method Overloading");
-            System.out.println("2. Constructor Overloading");
-            System.out.println("3. Use of this Keyword");
-            System.out.println("4. Object Passed as Parameter");
-            System.out.println("5. Call By Value");
-            System.out.println("6. Call By Reference");
-            System.out.println("7. Pattern");
-            System.out.println("8. Fibonacci");
-            System.out.println("0. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
+            if (choice == -1) {
+              System.out.println("\n===== MENU =====");
+              System.out.println("1. Method Overloading");
+              System.out.println("2. Constructor Overloading");
+              System.out.println("3. Use of this Keyword");
+              System.out.println("4. Object Passed as Parameter");
+              System.out.println("5. Call By Value");
+              System.out.println("6. Call By Reference");
+              System.out.println("7. Pattern");
+              System.out.println("8. Fibonacci");
+              System.out.println("0. Exit");
+              System.out.print("Enter your choice: ");
+              choice = scanner.nextInt();
+            }
 
             switch (choice) {
                 case 1:
+                    System.out.println("Method Overloading Output:");
                     System.out.println(methodOverload.area(10, 20));
                     System.out.println(methodOverload.area());
                     break;
                 case 2:
                     Overload player1 = new Overload();
                     Overload player2 = new Overload(200, 200, 5);
-                    System.out.println("Player 1 -> HP: " + player1.healthpoint + ", Mana: " + player1.mana
-                            + ", Stamina: " + player1.stamina);
-                    System.out.println("Player 2 -> HP: " + player2.healthpoint + ", Mana: " + player2.mana
-                            + ", Stamina: " + player2.stamina);
+                    System.out.println("Player 1 -> HP: " + player1.healthpoint + ", Mana: " + player1.mana + ", Stamina: " + player1.stamina);
+                    System.out.println("Player 2 -> HP: " + player2.healthpoint + ", Mana: " + player2.mana + ", Stamina: " + player2.stamina);
                     break;
                 case 3:
                     thisKeyword hello = new thisKeyword("Aditya");
-                    System.out.println(hello.name);
                     hello.Greet();
                     break;
                 case 4:
-                    objParameter.Greeting person1 = new objParameter.Greeting("Aditya");
-                    objParameter.Greeting person2 = new objParameter.Greeting("Pranjul");
+                    Greeting person1 = new Greeting("Aditya");
+                    Greeting person2 = new Greeting("Pranjul");
                     person1.Greet(person2);
                     break;
                 case 5:
                     int count = 10;
                     callByValue.increment(count);
-                    System.out.println("Value after method: " + count);
+                    System.out.println("Value after method (Call by Value): " + count);
                     break;
                 case 6:
                     callByReference myCounter = new callByReference(10);
                     callByReference.increment(myCounter);
-                    System.out.println("Value after method: " + myCounter.value);
+                    System.out.println("Value after method (Call by Reference): " + myCounter.value);
                     break;
                 case 7:
                     System.out.print("Enter number of rows: ");
                     int rows = scanner.nextInt();
                     for (int i = 1; i <= rows; i++) {
-                        int num;
-                        if (i % 2 != 0) {
-                            num = 1;
-                        } else {
-                            num = 2;
-                        }
+                        int num = (i % 2 != 0) ? 1 : 2;
                         for (int j = 1; j <= i; j++) {
                             System.out.print(num + " ");
                             num += 2;
@@ -158,17 +170,28 @@ public class Assignment_2_162502 {
                 case 8:
                     System.out.print("Enter the number of terms: ");
                     int n = scanner.nextInt();
-                    for (int i = 0; i < n; i++) {
-                        System.out.print(Fibonacci.fib(i) + " ");
-                    }
+                    for (int i = 0; i < n; i++) System.out.print(Fibonacci.fib(i) + " ");
                     System.out.println();
+                    break;
+                case 9:
+                    scanner.nextLine();
+                    System.out.print("Enter name for shouting: ");
+                    String shouter = scanner.nextLine();
+                    Outer myOuter = new Outer();
+                    Outer.Inner Mic = myOuter.new Inner();
+                    Mic.Shout(shouter);
                     break;
                 case 0:
                     System.out.println("Exiting...");
                     break;
                 default:
-                    System.out.println("Invalid choice. Try again.");
+                    System.out.println("Invalid choice.");
             }
+
+            if (choice != 0) {
+              choice = -1;
+            }
+
         } while (choice != 0);
 
         scanner.close();
